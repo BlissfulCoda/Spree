0ff98b36fe4567cb2b225a78f2a4e09fb0646c2d
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-11-06 04:45:53 +0000

    added react-router and a collections page to display items preview items

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 
-const Accessories = () => (
-  <div>
-    <h1>Accessories</h1>
-  </div>
-);
+// Imports from other Components
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/accessories" component={Accessories} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
