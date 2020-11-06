881f7c560a8199131a912a126f0b341ca39002d6
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-11-06 01:20:06 +0000

    updated the menu-item to navigate withRouter HOC

--- src/components/directory/directory.comonent.jsx
+++ src/components/directory/directory.comonent.jsx
@@ -13,31 +13,36 @@ class Directory extends Component {
           title: 'Accessories',
           imageUrl: 'https://i.ibb.co/BwmRPMg/stil-D4j-Raha-Ua-Ic-unsplash.jpg',
           id: 1,
-          size: 'large'
+          size: 'large',
+          linkUrl: 'accessories'
         },
         {
           title: 'Coats & Jackets',
           imageUrl: 'https://i.ibb.co/T4HcPnh/john-arano-i7c-DFWI7q-OU-unsplash.jpg',
           id: 2,
           size: 'large',
+          linkUrl: ''
         },
         {
           title: 'Shoes',
           imageUrl: 'https://i.ibb.co/Jx1nQY9/satria-aditya-f8v-EFZ7y-Pu-Y-unsplash.jpg',
           id: 3,
           size: 'large',
+          linkUrl: ''
         },
         {
           title: 'Womens',
           imageUrl: 'https://i.ibb.co/b3njksW/james-barr-Zw-Qw-K9g-R58s-unsplash.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: ''
         },
         {
           title: 'Mens',
           imageUrl: 'https://i.ibb.co/Jvwf0CS/sorin-sirbu-if-Yt0s-Kd-JYk-unsplash.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ''
         }
       ]
     };
@@ -46,8 +51,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, id, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...OthersectionProps}) => (
+          <MenuItem key={id} {...OthersectionProps}/>
         ))}
       </div>
     );
