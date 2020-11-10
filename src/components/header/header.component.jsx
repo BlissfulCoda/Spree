0ebfbe819be516b1d95b0b4dce7bf2b175c9e15f
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-11-10 03:44:34 +0000

    created a shopping cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 import { ReactComponent as Logo } from '../../assets/crown (3).svg';
 
 import './header.styles.scss';
@@ -31,13 +32,14 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+
+      <CartIcon />
     </div>
   </div>
 );
 
 const mapStateToProps = state => ({
-  currentUser:  state.user.currentUser
-})
-
+  currentUser: state.user.currentUser
+});
 
 export default connect(mapStateToProps)(Header);
