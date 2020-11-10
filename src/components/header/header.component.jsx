cbf0c618b321217ff01b8894ed581184df1f751b
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-11-10 03:56:42 +0000

    added a cart-dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import { ReactComponent as Logo } from '../../assets/crown (3).svg';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
@@ -35,6 +36,7 @@ const Header = ({ currentUser }) => (
 
       <CartIcon />
     </div>
+    <CartDropDown />
   </div>
 );
 
