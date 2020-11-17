8d754be88b43940b3965f936ad4b271f75862103
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-11-17 21:23:59 +0000

    compressed photos

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,39 +2,35 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'Accessories',
-      imageUrl: 'https://i.ibb.co/BwmRPMg/stil-D4j-Raha-Ua-Ic-unsplash.jpg',
+      imageUrl: 'https://i.ibb.co/mSBNbX2/accessories.jpg',
       id: 1,
       size: 'large',
       linkUrl: 'shop/accessories'
     },
     {
       title: 'Coats & Jackets',
-      imageUrl:
-        'https://i.ibb.co/T4HcPnh/john-arano-i7c-DFWI7q-OU-unsplash.jpg',
+      imageUrl: 'https://i.ibb.co/1vhB0qb/jackts.jpg',
       id: 2,
       size: 'large',
       linkUrl: 'shop/jackets'
     },
     {
       title: 'Shoes',
-      imageUrl:
-        'https://i.ibb.co/Jx1nQY9/satria-aditya-f8v-EFZ7y-Pu-Y-unsplash.jpg',
+      imageUrl: 'https://i.ibb.co/RB3hQ0x/shoes.jpg',
       id: 3,
       size: 'large',
       linkUrl: 'shop/shoes'
     },
     {
       title: 'Womens',
-      imageUrl:
-        'https://i.ibb.co/b3njksW/james-barr-Zw-Qw-K9g-R58s-unsplash.jpg',
+      imageUrl: 'https://i.ibb.co/TvddPNG/womens.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'Mens',
-      imageUrl:
-        'https://i.ibb.co/Jvwf0CS/sorin-sirbu-if-Yt0s-Kd-JYk-unsplash.jpg',
+      imageUrl: 'https://i.ibb.co/mzDGvyY/mens.jpg',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -7,76 +7,68 @@ const SHOP_DATA = {
       {
         id: 1,
         name: 'Headphones & Ipod',
-        imageUrl:
-          'https://i.ibb.co/z6d31Yt/90-angle-1i-Em-Bpy-2-s-unsplash.jpg',
-        price: 25
+        imageUrl: 'https://i.ibb.co/cT8DxS6/headphones-Ipod.jpg',
+        price: 350
       },
       {
         id: 2,
         name: 'Arno Senon',
-        imageUrl:
-          'https://i.ibb.co/vkLtMnh/arno-senoner-HFE2-Ry-C76tw-unsplash.jpg',
-        price: 18
+        imageUrl: 'https://i.ibb.co/f1grpBf/arno-senoner.jpg',
+        price: 640
       },
       {
         id: 3,
         name: 'Blue Earrings ',
-        imageUrl: 'https://i.ibb.co/FYK0YKh/pexels-ashithosh-u-1413420.jpg',
-        price: 35
+        imageUrl: 'https://i.ibb.co/27bgz9h/blue-Earrings.jpg',
+        price: 204
       },
       {
         id: 4,
         name: 'Snapback Hat ',
-        imageUrl: 'https://i.ibb.co/kgt8rX4/pexels-erik-mclean-4061512.jpg',
-        price: 18
+        imageUrl: 'https://i.ibb.co/kqy7Lmq/snapback-And-Gold-Chain.jpg',
+        price: 45
       },
       {
         id: 5,
         name: 'Gold & Silver Amachi',
-        imageUrl:
-          'https://i.ibb.co/YhzcsbB/content-pixie-ZB4e-Qc-Nq-VUs-unsplash.jpg',
-        price: 14
+        imageUrl: 'https://i.ibb.co/GJQLmvv/Gold-Silver-Amachi.jpg',
+        price: 550
       },
       {
         id: 6,
         name: 'Clear Glasses',
-        imageUrl:
-          'https://i.ibb.co/znY6GCm/jess-bailey-We-Co-Lo0-Rxp4-unsplash.jpg',
-        price: 18
+        imageUrl: 'https://i.ibb.co/L5Ht3pB/clear-Glasses.jpg',
+        price: 208
       },
       {
         id: 7,
         name: 'Loui Vitton Bag',
-        imageUrl:
-          'https://i.ibb.co/xGXpwQf/korie-cull-Om-IEij4-Mhn-A-unsplash.jpg',
-        price: 14
+        imageUrl: 'https://i.ibb.co/b6B14P7/Loui-Vitton-Bag.jpg',
+        price: 750
       },
       {
         id: 8,
         name: 'Arno Senoner',
-        imageUrl:
-          'https://i.ibb.co/BybhhTK/arno-senoner-ZT16-Yk-AYueo-unsplash.jpg',
-        price: 16
+        imageUrl: 'https://i.ibb.co/72jWGfn/arno-orange-bag.jpg',
+        price: 540
       },
       {
         id: 9,
         name: 'Diamond Earrings ',
-        imageUrl:
-          'https://i.ibb.co/R65RzSw/pexels-danielle-de-angelis-2849743.jpg',
-        price: 43
+        imageUrl: 'https://i.ibb.co/djK4Q7v/diamond-Earrings.jpg',
+        price: 450
       },
       {
         id: 10,
         name: 'Gold Earrings ',
-        imageUrl: 'https://i.ibb.co/JjVqHkK/pexels-dima-valkov-3266700.jpg',
-        price: 104
+        imageUrl: 'https://i.ibb.co/C6t6ssQ/gold-Earrings.jpg',
+        price: 500
       },
       {
         id: 11,
         name: 'Green RayBans',
-        imageUrl:
-          'https://i.ibb.co/DKWkr8r/charles-deluvio-1-nx1-QR5d-TE-unsplash.jpg',
-        price: 73
+        imageUrl: 'https://i.ibb.co/5TrFHGs/raybans.jpg',
+        price: 165
       }
     ]
   },
@@ -88,55 +80,51 @@ const SHOP_DATA = {
       {
         id: 12,
         name: 'Arnel-Hasanovic Boots',
-        imageUrl:
-          'https://i.ibb.co/k58f359/arnel-hasanovic-WBI5-XGIZugo-unsplash.jpg',
-        price: 89
+        imageUrl: 'https://i.ibb.co/NZ7MzHR/Arnel-Hasanovic-Boots.jpg',
+        price: 390
       },
       {
         id: 13,
 
         name: 'Pink Shoes',
-        imageUrl:
-          'https://i.ibb.co/sjLcwPS/sladjana-karvounis-KHKfdl-Ryt-As-unsplash.jpg',
-        price: 140
+        imageUrl: 'https://i.ibb.co/q9QYyt8/Pink-Shoes.jpg',
+        price: 340
       },
       {
         id: 14,
         name: 'Dr Martins',
-        imageUrl: 'https://i.ibb.co/9GRNF9n/pexels-fotografierende-1159670.jpg',
+        imageUrl: 'https://i.ibb.co/yBK6DS6/Doc-Martins.jpg',
         price: 260
       },
       {
         id: 15,
         name: 'Melvin-buezo Trainers',
-        imageUrl: 'https://i.ibb.co/6HjSTRP/pexels-melvin-buezo-2529148.jpg',
+        imageUrl: 'https://i.ibb.co/9cJgt5g/Nike-highs.jpg',
         price: 175
       },
       {
         id: 16,
         name: 'Terje-Sollie Shoes',
-        imageUrl: 'https://i.ibb.co/z6ZZBvj/pexels-terje-sollie-298863.jpg',
+        imageUrl: 'https://i.ibb.co/gDfG7tJ/office-shoes.jpg',
         price: 160
       },
       {
         id: 17,
         name: 'Nike Running Shoes',
-        imageUrl: 'https://i.ibb.co/9WsXjy6/revolt-164-6w-VEHf-I-unsplash.jpg',
-        price: 64
+        imageUrl: 'https://i.ibb.co/V06cF8b/Nike-running-shoes.jpg',
+        price: 164
       },
       {
         id: 18,
-        name: 'Nike Air Force One',
-        imageUrl:
-          'https://i.ibb.co/T1sjwMw/ryan-plomp-PGTO-A0e-Lt4-unsplash.jpg',
-        price: 100
+        name: 'Nike Air',
+        imageUrl: 'https://i.ibb.co/Pm06ZF2/Webp-net-compress-image-3.jpg',
+        price: 140
       },
       {
         id: 19,
         name: 'Emily-Gouker Shoes',
-        imageUrl:
-          'https://i.ibb.co/Z8czDMS/emily-gouker-Zx76sb-Andc0-unsplash.jpg',
-        price: 200
+        imageUrl: 'https://i.ibb.co/hYvGqgW/brown-shoes.jpg',
+        price: 450
       }
     ]
   },
@@ -148,33 +136,31 @@ const SHOP_DATA = {
       {
         id: 20,
         name: 'Brown Coat',
-        imageUrl:
-          'https://i.ibb.co/QkNwRwj/pexels-andrea-piacquadio-837140.jpg',
+        imageUrl: 'https://i.ibb.co/h96C6DP/brown-coat.jpg',
         price: 215
       },
       {
         id: 21,
         name: 'Green Winter Jacket',
-        imageUrl: 'https://i.ibb.co/r6S2fwT/pexels-unpetitvoyou-3317133.jpg',
+        imageUrl: 'https://i.ibb.co/SX2h9Dt/Green-Winter-Jacket.jpg',
         price: 190
       },
       {
         id: 22,
         name: "Women's Brown Coat",
-        imageUrl:
-          'https://i.ibb.co/nMLmXrC/pexels-apostolos-vamvouras-2282490.jpg',
+        imageUrl: 'https://i.ibb.co/n3M0HsY/womens-brown-jacket.jpg',
         price: 150
       },
       {
         id: 23,
         name: 'Long Grey Coat',
-        imageUrl: 'https://i.ibb.co/MMvVvrg/pexels-luca-nardone-3651597.jpg',
+        imageUrl: 'https://i.ibb.co/HD0KjSY/mens-long-coat.jpg',
         price: 165
       },
       {
         id: 24,
         name: "Women's Fur Coat",
-        imageUrl: 'https://i.ibb.co/k8pyyJH/pexels-oleg-magni-1734181.jpg',
+        imageUrl: 'https://i.ibb.co/Yy71qd1/womens-fur-coat.jpg',
         price: 185
       }
     ]
@@ -187,51 +173,45 @@ const SHOP_DATA = {
       {
         id: 25,
         name: 'Orange Tracksuit ',
-        imageUrl:
-          'https://i.ibb.co/Gv387Mq/dom-hill-nim-El-Tc-TNy-Y-unsplash.jpg',
-        price: 25
+        imageUrl: 'https://i.ibb.co/GVyLJny/womens-tracksuit.jpg',
+        price: 560
       },
       {
         id: 26,
         name: 'Top and Shorts',
-        imageUrl:
-          'https://i.ibb.co/1MmgH8N/alex-blajan-6ozh-V5b9ims-unsplash.jpg',
-        price: 20
+        imageUrl: 'https://i.ibb.co/F6yCM94/women1.jpg',
+        price: 120
       },
       {
         id: 27,
         name: 'Top and Short Jeans',
-        imageUrl:
-          'https://i.ibb.co/6JykCtQ/calvin-lupiya-y-Pg8cus-GD8-unsplash.jpg',
-        price: 80
+        imageUrl: 'https://i.ibb.co/xFS4Fys/women3.jpg',
+        price: 280
       },
       {
         id: 28,
         name: 'Red Summer Dress',
-        imageUrl:
-          'https://i.ibb.co/LhPDLbR/naeim-jafari-6-Xai7-Xx-Og-Bc-unsplash.jpg',
-        price: 80
+        imageUrl: 'https://i.ibb.co/G0jXwZT/red-summer-dress.jpg',
+        price: 450
       },
       {
         id: 29,
         name: 'Lily Grey Outfit',
         imageUrl:
-          'https://i.ibb.co/XbVQW0c/judeus-samson-0-UECc-Inu-CR4-unsplash.jpg',
-        price: 45
+          'https://i.ibb.co/s5j85D2/judeus-samson-0-UECc-Inu-CR4-unsplash.jpg',
+        price: 145
       },
       {
         id: 30,
         name: 'Joseph Kellner Top and Skirt',
-        imageUrl:
-          'https://i.ibb.co/r7TTYPv/joseph-kellner-Gh3m-p1-Jb-OE-unsplash.jpg',
-        price: 135
+        imageUrl: 'https://i.ibb.co/Lvf4w5x/womens4.jpg',
+        price: 235
       },
       {
         id: 31,
         name: 'Green Mini-Top',
-        imageUrl:
-          'https://i.ibb.co/WDH6TNY/atikh-bana-Ka-MTEm-Jnx-Y-unsplash.jpg',
-        price: 50
+        imageUrl: 'https://i.ibb.co/C5yk1Pm/women2.jpg',
+        price: 60
       }
     ]
   },
@@ -243,42 +223,38 @@ const SHOP_DATA = {
       {
         id: 32,
         name: 'Marron Suit',
-        imageUrl:
-          'https://i.ibb.co/j5FpKnj/pexels-the-lazy-artist-gallery-1300550.jpg',
+        imageUrl: 'https://i.ibb.co/sKhRq6t/Maroon-suit.jpg',
         price: 325
       },
       {
         id: 33,
         name: 'Halloween Shirt',
-        imageUrl:
-          'https://i.ibb.co/MZk2Hdv/ian-dooley-wt-Fc4o4-Bir8-unsplash.jpg',
-        price: 20
+        imageUrl: 'https://i.ibb.co/PjNKhrp/Halloween-Shirt.jpg',
+        price: 145
       },
       {
         id: 34,
         name: 'Black T',
-        imageUrl:
-          'https://i.ibb.co/CBfb5C0/pexels-cristian-benavides-3662357.jpg',
-        price: 45
+        imageUrl: 'https://i.ibb.co/4tVyD6m/black-T.jpg',
+        price: 55
       },
       {
         id: 35,
         name: 'Yellow Tracksuit',
-        imageUrl:
-          'https://i.ibb.co/vcqRtcW/pexels-visio-n-de-enfoque-3341231.jpg',
-        price: 105
+        imageUrl: 'https://i.ibb.co/TqRHMFN/Yellow-Tracksuit.jpg',
+        price: 250
       },
       {
         id: 36,
         name: 'Red Shirt',
-        imageUrl: 'https://i.ibb.co/98Yds6G/pexels-yogendra-singh-1760900.jpg',
-        price: 80
+        imageUrl: 'https://i.ibb.co/KXFBkZ0/red-Shirt.jpg',
+        price: 160
       },
       {
         id: 37,
         name: 'White Shirt',
-        imageUrl: 'https://i.ibb.co/Xt5Sh2d/pexels-ralph-rabago-3214769.jpg',
-        price: 45
+        imageUrl: 'https://i.ibb.co/LNP5Xwt/white-T.jpg',
+        price: 145
       }
     ]
   }
